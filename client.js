26094612350ebe3dd91eac3d71201975ddb90018
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-07-10 14:03:42 -1000

    cleaned up code.  working on setting dynamic port

--- client.js
+++ client.js
@@ -20,31 +20,7 @@ var socket = net.createConnection({
 
 socket.on(SOCKET_CONNECT, function() {
 
-  process.argv.forEach(function(val, index, array) {
-    if (/(-p)/g.exec(val)) {
-      if(/\d{4}/g.exec(val)){
-
-        var grabPort = val;
-        console.log('grabPort', grabPort);
-      }
-    }
-    var grabHost = /(\bl\w+:\d+\/)/g.exec(val);
-    var grabRequestedType = /(-h\b|-b\b)/g.exec(val);
-    var grabRequestedFile = /\(?(?:(http|https|ftp):\/\/)?(?:((?:[^\W\s]|\.|-|[:]{1})+)@{1})?((?:www.)?(?:[^\W\s]|\.|-)+[\.][^\W\s]{2,4}|localhost(?=\/)|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::(\d*))?([\/]?[^\s\?]*[\/]{1})*(?:\/?([^\s\n\?\[\]\{\}\#]*(?:(?=\.)){1}|[^\s\n\?\[\]\{\}\.\#]*)?([\.]{1}[^\s\?\#]*)?)?(?:\?{1}([^\s\n\#\[\]]*))?([\#][^\s\n]*)?\)?/.exec(val);
-    if (grabRequestedType) {
-      SOCKET_REQUEST_TYPE = grabRequestedType;
-    }
-
-    if (grabRequestedFile && grabRequestedFile[0] !== 'client.js') {
-      SOCKET_REQUESTED_FILE = grabRequestedFile[0];
-    }
-
-    if (grabHost) {
-      SOCKET_HOST_NAME = grabHost[0]
-    }
-
-  });
-
+  processArgs();
 
   SOCKET_REQUESTED_FILE = SOCKET_REQUESTED_FILE || '/';
   if (!SOCKET_REQUEST_TYPE) {
@@ -66,6 +42,33 @@ socket.on(SOCKET_CONNECT, function() {
   }
 
 });
+
+function processArgs() {
+  process.argv.forEach(function(val, index, array) {
+    if (/(-p)/g.exec(val)) {
+      if (/\d{4}/g.exec(val)) {
+
+        var grabPort = val;
+        console.log('grabPort', grabPort);
+      }
+    }
+    var grabHost = /(\bl\w+:\d+\/)/g.exec(val);
+    var grabRequestedType = /(-h\b|-b\b)/g.exec(val);
+    var grabRequestedFile = /\(?(?:(http|https|ftp):\/\/)?(?:((?:[^\W\s]|\.|-|[:]{1})+)@{1})?((?:www.)?(?:[^\W\s]|\.|-)+[\.][^\W\s]{2,4}|localhost(?=\/)|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::(\d*))?([\/]?[^\s\?]*[\/]{1})*(?:\/?([^\s\n\?\[\]\{\}\#]*(?:(?=\.)){1}|[^\s\n\?\[\]\{\}\.\#]*)?([\.]{1}[^\s\?\#]*)?)?(?:\?{1}([^\s\n\#\[\]]*))?([\#][^\s\n]*)?\)?/.exec(val);
+    if (grabRequestedType) {
+      SOCKET_REQUEST_TYPE = grabRequestedType;
+    }
+
+    if (grabRequestedFile && grabRequestedFile[0] !== 'client.js') {
+      SOCKET_REQUESTED_FILE = grabRequestedFile[0];
+    }
+
+    if (grabHost) {
+      SOCKET_HOST_NAME = grabHost[0]
+    }
+
+  });
+}
 
 function generateRequest(requestedFile, requestType) {
   var requestResponse = {
